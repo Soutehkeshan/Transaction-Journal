dbe3a643cfca4e5b203a1acf50a016171efe1913
Arad Soutehkeshan <arad.skn84@gmail.com>
2025-06-06 21:18:56 +0100

    Add get by id and symbol methods

--- database/db_utils.py
+++ database/db_utils.py
@@ -6,3 +6,19 @@ print(f"DB_PATH: {DB_PATH}")
 
 def get_db_connection():
     return sqlite3.connect(DB_PATH)
+
+def get_asset_id_by_symbol(symbol: str) -> int | None:
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT id FROM assets WHERE symbol = ?", (symbol.upper(),))
+    row = cursor.fetchone()
+    conn.close()
+    return row[0] if row else None
+
+def get_symbol_by_asset_id(id: int) -> str | None:
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT symbol FROM assets WHERE id = ?", (id,))
+    row = cursor.fetchone()
+    conn.close()
+    return row[0] if row else None
