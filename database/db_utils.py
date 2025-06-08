98b54a714c9a7c88d6f8e474365ee02d4aa4617c
Arad Soutehkeshan <arad.skn84@gmail.com>
2025-06-08 17:19:07 +0100

    Make compatibility with python3.8

--- database/db_utils.py
+++ database/db_utils.py
@@ -1,5 +1,6 @@
 import sqlite3
 import os
+from typing import Optional
 
 DB_PATH = os.path.join(os.path.dirname(__file__), "transactions.db")
 print(f"DB_PATH: {DB_PATH}")
@@ -7,7 +8,7 @@ print(f"DB_PATH: {DB_PATH}")
 def get_db_connection():
     return sqlite3.connect(DB_PATH)
 
-def get_asset_id_by_symbol(symbol: str) -> int | None:
+def get_asset_id_by_symbol(symbol: str) -> Optional[int]:
     conn = get_db_connection()
     cursor = conn.cursor()
     cursor.execute("SELECT id FROM assets WHERE symbol = ?", (symbol.upper(),))
@@ -15,7 +16,7 @@ def get_asset_id_by_symbol(symbol: str) -> int | None:
     conn.close()
     return row[0] if row else None
 
-def get_symbol_by_asset_id(id: int) -> str | None:
+def get_symbol_by_asset_id(id: int) -> Optional[str]:
     conn = get_db_connection()
     cursor = conn.cursor()
     cursor.execute("SELECT symbol FROM assets WHERE id = ?", (id,))
