d06ab9553a8151afa1b10b063fa024ec0cf9b5ae
Arad Soutehkeshan <arad.skn84@gmail.com>
2025-06-12 00:57:31 +0100

    Add a separate class for pop up messages

---
+++ views/PopUp.py
@@ -0,0 +1,18 @@
+from PyQt5.QtWidgets import QMessageBox
+
+class PopUp:
+    @staticmethod
+    def show_message(message: str, title: str = "پیام"):
+        msg_box = QMessageBox()
+        msg_box.setWindowTitle(title)
+        msg_box.setText(message)
+        msg_box.setIcon(QMessageBox.Information)
+        msg_box.exec_()
+
+    @staticmethod
+    def show_warning(message: str, title: str = "هشدار"):
+        msg_box = QMessageBox()
+        msg_box.setWindowTitle(title)
+        msg_box.setText(message)
+        msg_box.setIcon(QMessageBox.Warning)
+        msg_box.exec_()
