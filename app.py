d45abfb1dd4519bbb86e353e6d539bf451df6374
Arad Soutehkeshan <arad.skn84@gmail.com>
2025-06-12 01:50:44 +0100

    Add font

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 import sys
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QFontDatabase, QFont
 from controllers.main_controller import MainController
 
 def main():
     app = QApplication(sys.argv)
+
+    # Load custom Persian font
+    font_id = QFontDatabase.addApplicationFont("resources/fonts/Far_Nazanin.ttf")
+    if font_id != -1:
+        families = QFontDatabase.applicationFontFamilies(font_id)
+        if families:
+            app.setFont(QFont(families[0]))
     
     # Initialize main controller
     main_controller = MainController()
